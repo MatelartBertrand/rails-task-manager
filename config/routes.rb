b439b82d7f15791256c1fc91413779aeb1657933
Bertrand Matelart <matelart.bertrand@gmail.com>
2017-08-08 12:09:41 +0200

    add Task_controller + Views + route in Routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  #real all
+  get    "tasks",             to: "tasks#index"
+  #read one by id
+  get    "tasks/:id",         to: "tasks#show"
+  #create (send formulaire + recupération de la donnée)
+  get    "tasks/new",         to: "tasks#new"
+  post   "tasks",             to: "tasks#create"
+  #update (send formulaire + récupération de la donnée)
+  get    "tasks/:id/edit",    to: "tasks#edit"
+  patch  "tasks/:id",         to: "tasks#update"
+  #destroy by id
+  delete "tasks/:id",         to: "tasks#destroy"
+
 end
+
