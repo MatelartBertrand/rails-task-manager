6cb61f6e82d7854aefe42d70161e84160464cac9
Bertrand Matelart <matelart.bertrand@gmail.com>
2017-08-08 17:28:41 +0200

    machine fonctionnelle

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
 
+  resources :tasks
+  # , only: [:create, :index, :destroy]
 
-  #real all
-  get    "tasks",             to: "tasks#index"
-  #read one by id
-  get    "tasks/:id",         to: "tasks#show"
-  #create (send formulaire + recupération de la donnée)
-  get    "tasks/new",         to: "tasks#new"
-  post   "tasks",             to: "tasks#create"
-  #update (send formulaire + récupération de la donnée)
-  get    "tasks/:id/edit",    to: "tasks#edit"
-  patch  "tasks/:id",         to: "tasks#update"
-  #destroy by id
-  delete "tasks/:id",         to: "tasks#destroy"
+  # #real all
+  # get    "tasks",             to: "tasks#index"
+  # #read one by id
+  # get    "tasks/:id",         to: "tasks#show"
+  # #create (send formulaire + recupération de la donnée)
+  # get    "tasks/new",         to: "tasks#new"
+  # post   "tasks",             to: "tasks#create"
+  # #update (send formulaire + récupération de la donnée)
+  # get    "tasks/:id/edit",    to: "tasks#edit"
+  # patch  "tasks/:id",         to: "tasks#update"
+  # #destroy by id
+  # delete "tasks/:id",         to: "tasks#destroy"
 
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.destroy_all
+Task.create!(title: 'test1', detail: 'blablabla')
+Task.create!(title: 'test2', detail: 'blablabla')
+Task.create!(title: 'test3', detail: 'blablabla')
+Task.create!(title: 'test4', detail: 'blablabla')
+Task.create!(title: 'test5', detail: 'blablabla')
+Task.create!(title: 'test6', detail: 'blablabla')
+
+
+
